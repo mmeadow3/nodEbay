7cfc69935f8e996cd76884ade8996204771df1b6
Mike Meadows <mmeadow33@gmail.com>
2016-10-27 23:06:47 -0500

    logic now working for winnig bid to move item to user page

--- client/app/controllers/UserPage.js
+++ client/app/controllers/UserPage.js
@@ -9,29 +9,10 @@ app.controller("UserPage", function($scope, UserFactory) {
       $scope.user = user
       ///////beaking out each individual item from Array////////////
         user.itemsWon.forEach((item) => {
-        itemWon.push(item);
-        $scope.itemWon = itemWon;
+          itemWon.push(item);
+          $scope.itemWon = itemWon;
       })
     })
   }
   getUserData()
 })
-
-
-//
-// app.controller("AllItemsCtrl", function($scope, ItemFactory) {
-//   $scope.itemsForSale = [];
-//   const getAllItems = () => {
-//     ItemFactory.getItems()
-//     .then(items => {
-//       items.forEach((item) => {
-//         if (item.available === true) {
-//         $scope.itemsForSale.push({name: item.name, price: item.price, available: "For Sale"})
-//       } else {
-//         $scope.itemsForSale.push({name: item.name, price: item.price, available: "Sold"})
-//       }
-//       })
-//     })
-//   }
-//   getAllItems();
-// })
